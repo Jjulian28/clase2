974fb27208c9ba18c896fd31d2407cd72f41d709
Julian <yairtorres219@unisangil.edu.co>
2024-11-01 19:46:34 -0500

    primer cargue

---
+++ src/Main.kt
@@ -0,0 +1,48 @@
+fun main() {
+
+    // comentarios de 1 linea
+    /*
+    comentarios multilinea
+
+     */
+
+    var numero = 45 //variable de tipo entero
+    var decimal = 12.5 //
+    var texto = "Hola mundo"
+    val fecha = "2024-10-15"
+    val pi = 3.1416
+
+    println("El numero entero es: ${numero}")
+    println("El numero decimal es: ${decimal}")
+    println("El texto es: ${texto}")
+    println("La primera constante es: ${fecha}")
+    println("El numero PI es: ${pi}")
+
+
+    // Variable explicitas
+
+    var numero1: Int =56
+    var resultado:Double = 25.8
+    var caracter: Char = 'a'
+
+    println("La primer variable explicita es: ${numero1}")
+    println("La segunda variable explicita es: ${resultado}")
+    println("La tercera variable explicita es: ${caracter}")
+
+
+    // CONVERSIONES ENTRE DATOS
+
+    var total ="10"
+
+    var convertir = total.toInt()*20
+
+    println("EL VALOR CONVERTIDO ES: ${convertir} ")
+
+
+
+
+
+
+
+
+}
